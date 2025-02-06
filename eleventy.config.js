eb73dc73bb0a7fab1a27defcc94e018da6271e4a
Michael Healy <mhealy2011@gmail.com>
2025-02-06 10:29:01 -0600

    Updating Content

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,3 +1,10 @@
-module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("bundle.css");
-  };
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("./src/bundle.css");
+    eleventyConfig.addPassthroughCopy("./src/img");
+};
+
+module.exports.config = {
+    dir: {
+        input: "src",
+    },
+};
